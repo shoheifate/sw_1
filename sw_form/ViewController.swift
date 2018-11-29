6f321b25cff6b10d0faffd99bf16b198e7b38929
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2018-11-29 10:46:56 +0900

    Initial Commit

---
+++ sw_form/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sw_form
+//
+//  Created by 玉城翔平 on 2018/11/29.
+//  Copyright © 2018 玉城翔平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ sw_formTests/sw_formTests.swift
@@ -0,0 +1,34 @@
+//
+//  sw_formTests.swift
+//  sw_formTests
+//
+//  Created by 玉城翔平 on 2018/11/29.
+//  Copyright © 2018 玉城翔平. All rights reserved.
+//
+
+import XCTest
+@testable import sw_form
+
+class sw_formTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
